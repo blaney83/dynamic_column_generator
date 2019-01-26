ed921162d9fd7a025dd47faca8fa9e35f67c5575
Blaney83 <professionallaney@gmail.com>
2019-01-26 16:00:23 -0700

    Basic testing completed. First functional KNIME node created, albiet not very useful, but proof of concept accomplished.

--- src/io/github/blaney83/DynamicColumnGeneratorNodeDialog.java
+++ src/io/github/blaney83/DynamicColumnGeneratorNodeDialog.java
@@ -2,10 +2,8 @@ package io.github.blaney83;
 
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
 import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
-import org.knime.core.node.defaultnodesettings.DialogComponentNumber;
 import org.knime.core.node.defaultnodesettings.DialogComponentString;
 import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
-import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
 
 /**
